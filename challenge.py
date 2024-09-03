9c33db1dd7c82ace51af65d2d919e773c80e0fd8
R-kjim <rkayjim@gmail.com>
2024-09-03 12:24:02 +0300

    working exapmles added

--- challenge.py
+++ challenge.py
@@ -2,17 +2,29 @@
 def add_numbers(num1,num2):
     return sum((num1,num2))
     
+#example
+print(add_numbers(10,17))
+
 #function that returns True/False if a number is even or not
 def is_even(num):
     return num%2==0
+#example
+print(is_even(27))
+print(is_even(22))
 
 #function that takes in a string and returns its reverse
 def reverse_string(string):
     return string[::-1]
 
+#example
+print(reverse_string("apple"))
+
 #functions that takes a string and returns the count of vowels in the string,ignoring case sensitivity
 def count_vowels(string):
     return len([vowel for vowel in string if vowel.lower() in("a","e","i","o","u")])
+
+#example
+print(count_vowels("elephant"))
 
 #function that takes in a non-negative integer and returns the factorial of the integer
 def calculate_factorial(num):
@@ -21,6 +33,8 @@ def calculate_factorial(num):
         for i in range(1,num+1):
             result=result*i
         return result
+#example
+print(calculate_factorial(5))
 
 #function that takes in a function and applies a decorator
 def apply_decorator(func):
@@ -28,6 +42,7 @@ def apply_decorator(func):
         print("Decorator Applied")
         return func(*args,**kwargs)
     return decorator_func
+
 #application example
 @apply_decorator
 def funct1(val):
@@ -37,10 +52,12 @@ funct1("Hello")
     #Decorator Applied
     #Hello after Decorator
 
-#function that sorts a list of tuples by values of a key age
+#function that sorts a list of tuples by values of a key in the tuple
 def sort_by_age(list_of_tuples):
     return sorted(list_of_tuples, key=lambda item:item[1])
 
+#example
+print(sort_by_age([("age",25),("age",85),("age",35),("age",32)]))
 
 #function that takes two dictionaries and merges them into a single diction,summing up any common values
 def merge_dicts(dict1,dict2):
@@ -51,6 +68,8 @@ def merge_dicts(dict1,dict2):
             dict5[key]=dict5.get(key,0)+value #assign a value of 0 to each key initially and increase it by value of the keys to add values of common keys
     return dict5
 
+#example
+print(merge_dicts({"a":2,"b":3,"c":4},{"b":2,"c":10,"d":9}))
 
 #define a class named car that takes in data and prints the car's info
 class Car:
@@ -64,3 +83,6 @@ class Car:
             print(f"Model:{self.model}")
             print(f"Year:{self.year}")
 
+#example
+subaru=Car("Subaru","Forster",2016)
+subaru.display_info()
